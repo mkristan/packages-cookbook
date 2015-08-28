0cd7ba247755b2083282f64f7b800ac78eb9ba26
Matt Ray <matthewhray@gmail.com>
2015-08-28 00:37:11 -0500

    fix tests and add coverage

---
+++ spec/unit/default_spec.rb
@@ -0,0 +1,75 @@
+require_relative '../spec_helper'
+
+describe 'packages::default' do
+  context 'packages attribute is an array' do
+    context 'default action is install' do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new do |node|
+          node.set['packages'] = %w(git)
+        end.converge(described_recipe)
+      end
+
+      it 'installs git' do
+        expect(chef_run).to install_package('git')
+      end
+    end
+
+    context 'default action is install multiple' do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new do |node|
+          node.set['packages'] = %w(bash openssl)
+        end.converge(described_recipe)
+      end
+
+      it 'installs bash and openssl' do
+        expect(chef_run).to install_package(%w(bash openssl))
+      end
+    end
+
+    context 'default action is upgrade' do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new do |node|
+          node.set['packages'] = %w(git)
+          node.set['packages_default_action'] = 'upgrade'
+        end.converge(described_recipe)
+      end
+
+      it 'upgrades git' do
+        expect(chef_run).to upgrade_package('git')
+      end
+    end
+
+    context 'default action is upgrade multiple' do
+      let(:chef_run) do
+        ChefSpec::SoloRunner.new do |node|
+          node.set['packages'] = %w(bash openssl)
+          node.set['packages_default_action'] = 'upgrade'
+        end.converge(described_recipe)
+      end
+
+      it 'upgrades bash and openssl' do
+        expect(chef_run).to upgrade_package(%w(bash openssl))
+      end
+    end
+  end
+
+  context 'packages attribute is a hash' do
+    let(:chef_run) do
+      ChefSpec::SoloRunner.new do |node|
+        node.set['packages'] = { 'bash' => 'install', 'openssl' => 'upgrade', 'xorg-common' => 'remove' }
+      end.converge(described_recipe)
+    end
+
+    it 'installs bash' do
+      expect(chef_run).to install_package('bash')
+    end
+
+    it 'upgrades openssl' do
+      expect(chef_run).to upgrade_package('openssl')
+    end
+
+    it 'removes xorg-common' do
+      expect(chef_run).to remove_package('xorg-common')
+    end
+  end
+end

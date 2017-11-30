25ca8d74f33d77ee29ccf5499aaa0e1276bca610
Matt Ray <matthewhray@gmail.com>
2017-11-30 12:02:54 +1100

    update tests to handle multipackage support

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -5,7 +5,7 @@ describe 'packages::default' do
     context 'default action is install' do
       let(:chef_run) do
         ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-          node.set['packages-cookbook'] = [git]
+          node.override['packages-cookbook'] = ['git']
         end.converge(described_recipe)
       end
 
@@ -17,7 +17,7 @@ describe 'packages::default' do
     context 'default action is install multiple' do
       let(:chef_run) do
         ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-          node.set['packages-cookbook'] = [bash, openssl]
+          node.override['packages-cookbook'] = %w( bash openssl )
         end.converge(described_recipe)
       end
 
@@ -29,7 +29,7 @@ describe 'packages::default' do
     context 'default action is install multiple separately' do
       let(:chef_run) do
         ChefSpec::SoloRunner.new(platform: 'mac_os_x', version: '10.12') do |node|
-          node.set['packages-cookbook'] = %w( bash openssl )
+          node.override['packages-cookbook'] = %w( bash openssl )
         end.converge(described_recipe)
       end
 
@@ -44,8 +44,8 @@ describe 'packages::default' do
     context 'default action is upgrade' do
       let(:chef_run) do
         ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-          node.set['packages-cookbook'] = [git]
-          node.set['packages-cookbook_default_action'] = 'upgrade'
+          node.override['packages-cookbook'] = ['git']
+          node.override['packages-cookbook_default_action'] = 'upgrade'
         end.converge(described_recipe)
       end
 
@@ -57,13 +57,13 @@ describe 'packages::default' do
     context 'default action is upgrade multiple' do
       let(:chef_run) do
         ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-          node.set['packages-cookbook'] = [bash, openssl]
-          node.set['packages-cookbook_default_action'] = 'upgrade'
+          node.override['packages-cookbook'] = %w( bash openssl)
+          node.override['packages-cookbook_default_action'] = 'upgrade'
         end.converge(described_recipe)
       end
 
       it 'upgrades bash and openssl' do
-        expect(chef_run).to upgrade_package([bash, openssl])
+        expect(chef_run).to upgrade_package(%w( bash openssl ))
       end
     end
   end
@@ -71,20 +71,26 @@ describe 'packages::default' do
   context 'packages attribute is a hash' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04') do |node|
-        node.set['packages-cookbook'] = { 'bash' => 'install', 'openssl' => 'upgrade', 'xorg-common' => 'remove' }
+        node.override['packages-cookbook'] = { 'bash' => 'install', 'openssl' => 'upgrade', 'xorg-common' => 'remove' }
       end.converge(described_recipe)
     end
 
     it 'installs bash' do
-      expect(chef_run).to install_package('bash')
+      expect(chef_run).to install_package(%w( bash openssl xorg-common ))
+      expect(chef_run).to_not install_package('openssl')
+      expect(chef_run).to_not install_package('xorg-common')
     end
 
     it 'upgrades openssl' do
-      expect(chef_run).to upgrade_package('openssl')
+      expect(chef_run).to upgrade_package(%w( bash openssl xorg-common ))
+      expect(chef_run).to_not upgrade_package('bash')
+      expect(chef_run).to_not upgrade_package('xorg-common')
     end
 
     it 'removes xorg-common' do
-      expect(chef_run).to remove_package('xorg-common')
+      expect(chef_run).to remove_package(%w( bash openssl xorg-common ))
+      expect(chef_run).to_not remove_package('bash')
+      expect(chef_run).to_not remove_package('openssl')
     end
   end
 end
